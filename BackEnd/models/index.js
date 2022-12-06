049fd6d7681d9c9d921ef1933388490fae76adfa
_devOck <respecttowardyou@gmail.com>
2022-12-06 14:41:57 +0900

    backend 기본

---
+++ BackEnd/models/index.js
@@ -0,0 +1,22 @@
+// const Sequelize = require("sequelize");
+// const env = process.env.NODE_ENV || "development";
+// const config = require("../config/config")[env];
+// // const User = require("./user");
+
+// const db = {};
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   config,
+//   { timezone: "+09:00" }
+// );
+
+// db.sequelize = sequelize;
+// // db.User = User;
+
+// User.init(sequelize);
+
+// User.associate(db);
+
+// module.exports = db;

88d8e7e878822c517eda9e516651cd5f68c5169e
_devOck <respecttowardyou@gmail.com>
2022-12-07 16:35:48 +0900

    Create part_default TB

--- BackEnd/models/part_default.js
+++ BackEnd/models/part_default.js
@@ -1,69 +1,32 @@
-// const Sequelize = require("sequelize");
+const Sequelize = require("sequelize");
 
-// module.exports = class User extends Sequelize.Model {
-//   static init(sequelize) {
-//     return super.init(
-//       {
-//         email: {
-//           type: Sequelize.STRING(40),
-//           allowNull: false,
-//         },
-//         type: {
-//           type: Sequelize.ENUM("free", "premium"),
-//           allowNull: false,
-//           defaultValue: "premium",
-//         },
-//         nick: {
-//           type: Sequelize.STRING(100),
-//           allowNull: false,
-//         },
-//         password: {
-//           type: Sequelize.STRING(100),
-//           allowNull: false,
-//         },
-//         gold: {
-//           type: Sequelize.INTEGER(40),
-//           allowNull: false,
-//           defaultValue: 100000,
-//         },
-//         currentShipImage: {
-//           type: Sequelize.STRING(100),
-//           allowNull: false,
-//           defaultValue: "rocket0.png",
-//         },
-//         provider: {
-//           type: Sequelize.STRING(10),
-//           allowNull: false,
-//           defaultValue: "local",
-//         },
-//         snsId: {
-//           type: Sequelize.STRING(30),
-//           allowNull: true,
-//         },
-//       },
-//       {
-//         sequelize,
-//         timestamps: true,
-//         underscored: false,
-//         modelName: "User",
-//         tableName: "Users",
-//         paranoid: true,
-//         charset: "utf8",
-//         collate: "utf8_general_ci",
-//       }
-//     );
-//   }
+module.exports = class Part_default extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        part_type: {
+          type: Sequelize.INTEGER(40),
+          allowNull: false,
+        },
+        max_life: {
+          type: Sequelize.INTEGER(40),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Part_default",
+        tableName: "part_defaults",
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
 
-//   static associate(db) {
-//     db.User.hasMany(db.Spaceship);
-//     db.User.hasMany(db.Scoredata);
-//     db.User.hasMany(db.Post, {
-//       onDelete: "CASCADE",
-//       hooks: true,
-//     });
-//     db.User.hasMany(db.Postlike, {
-//       onDelete: "CASCADE",
-//       hooks: true,
-//     });
-//   }
-// };
+  static associate(db) {
+    db.Part_default.belongsTo(db.Part);
+  }
+};

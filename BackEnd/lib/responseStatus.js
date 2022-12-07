7c95538cc7ad7b612b206cece34c794a2950a2c7
_devOck <respecttowardyou@gmail.com>
2022-12-07 16:29:43 +0900

    Update responseStatus Code

--- BackEnd/lib/responseStatus.js
+++ BackEnd/lib/responseStatus.js
@@ -3,63 +3,64 @@
 
 200
 success
-insufficient : 보낼 data가 없거나 부족할 때
 
-404
+204
 invalide : invalid email
 invalidp : invalid pw
 invalidu : invalid user	
 invalidt : invalid token
 different : different UserId
-expired : token 만료
+
 notenough : 원하는 data가 param이나 req에 아예 없거나 부족할 때
+invalid : req로 받은 data가 유효하지 않을 때
+expired : token 만료
+insufficient : 보낼 data가 없거나 부족할 때
 
 500
 server error
 */
 
-const resStatus = {
+exports.resStatus = {
   success: {
     code: 200,
     message: "success",
   },
-  insufficient: {
-    code: 200,
-    message: "nothing",
+
+  // invalide: {
+  //   code: 204,
+  //   message: "invalide",
+  // },
+  // invalidp: {
+  //   code: 204,
+  //   message: "invalidp",
+  // },
+  // invalidu: {
+  //   code: 204,
+  //   message: "invalidu",
+  // },
+  // invalidt: {
+  //   code: 204,
+  //   message: "invalidt",
+  // },
+  // different: {
+  //   code: 204,
+  //   message: "different",
+  // },
+
+  notenough: {
+    code: 204,
+    message: "notenough", // 원하는 data가 param이나 req에 아예 없거나 부족할 때
   },
-  invalide: {
-    code: 404,
-    message: "invalide",
-  },
-  invalidp: {
-    code: 404,
-    message: "invalidp",
-  },
-  invalidu: {
-    code: 404,
-    message: "invalidu",
-  },
-  invalidt: {
-    code: 404,
-    message: "invalidt",
-  },
-  different: {
-    code: 404,
-    message: "different",
+  invalid: {
+    code: 204,
+    message: "invalid", // req로 받은 data가 유효하지 않을 때
   },
   expired: {
-    code: 404,
-    message: "expired",
+    code: 204,
+    message: "expired", // token 만료
   },
-  notenough: {
-    code: 404,
-    message: "notenough",
-  },
-  blank: {
-    code: 404,
-    message: "blank",
+  insufficient: {
+    code: 204,
+    message: "nothing", // 보낼 data가 없거나 부족할 때
   },
 };
-
-// module.exports = { response, resStatus };
-module.exports = { resStatus };

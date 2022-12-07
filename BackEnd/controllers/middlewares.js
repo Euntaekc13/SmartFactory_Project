7857e7d59a1e587280ef12579e6d37e2bdb82066
_devOck <respecttowardyou@gmail.com>
2022-12-07 17:44:23 +0900

    Update middleware Code

--- BackEnd/controllers/middlewares.js
+++ BackEnd/controllers/middlewares.js
@@ -1,9 +1,5 @@
-const express = require("express");
 const jwt = require("jsonwebtoken");
-const RateLimit = require("express-rate-limit");
 const { resStatus } = require("../lib/responseStatus");
-
-const router = express.Router();
 
 // token 유효성 검사
 exports.verifyToken = (req, res, next) => {
@@ -34,34 +30,3 @@ exports.verifyToken = (req, res, next) => {
     });
   }
 };
-
-// game/gear 페이지 요청 횟수 제한(user type에 따라) (DoS 방지)
-exports.apiLimiter = router.use(async (req, res, next) => {
-  if (req.decoded.type === "premium") {
-    premiumApiLimiter(req, res, next);
-  } else {
-    freeApiLimiter(req, res, next);
-  }
-});
-
-const premiumApiLimiter = RateLimit({
-  windowMs: 60 * 1000, // 1분당
-  max: 100, // 최대 100번까지
-  handler(req, res) {
-    res.status(this.statusCode).json({
-      code: this.statusCode,
-      message: "premium limit",
-    });
-  },
-});
-
-const freeApiLimiter = RateLimit({
-  windowMs: 60 * 1000, // 1분당
-  max: 5, // 최대 5번까지
-  handler(req, res) {
-    res.status(this.statusCode).json({
-      code: this.statusCode,
-      message: "free limit",
-    });
-  },
-});

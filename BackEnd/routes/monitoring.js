e8fa00e6c72a7bcd23cea84dc9d92e4f4d8c3c3b
_devOck <respecttowardyou@gmail.com>
2022-12-07 17:46:27 +0900

    Update routes/monitoring Code

--- BackEnd/routes/monitoring.js
+++ BackEnd/routes/monitoring.js
@@ -5,7 +5,7 @@ const { verifyToken } = require("../controllers/middlewares");
 const { Test_result, Machine, Part, Part_default } = require("../models");
 
 // 공정 모니터링 정보 조회
-router.GET("/:MachineId", verifyToken, async (req, res, next) => {
+router.get("/:MachineId", verifyToken, async (req, res, next) => {
   console.log("GET /monitoring/:MachineId 진입");
   try {
     const MachineId = parseInt(req.params.MachineId, 10);
@@ -46,18 +46,20 @@ router.GET("/:MachineId", verifyToken, async (req, res, next) => {
 });
 
 // 공정 모니터링 실시간 정보 추가
-router.POST("/add/:MachineId", verifyToken, async (req, res, next) => {
-  console.log("POST /monitoring/add/:MachineId 진입");
-  console.log(req.body);
-  try {
-    const MachineId = parseInt(req.params.MachineId, 10);
-    /*
-    1) 1호기, 2호기, 3호기 각 결과값이 통과했는지 여부를 어디 DB에 update. 
-    2) final_result를 Test_result DB에 add.  
-   (1, 2, 3호기 part_status가 전부 ‘정상’이어야 final result가 pass.  / 아니면 fail 이 되도록 로직) 
-    */
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
+// router.post("/add/:MachineId", verifyToken, async (req, res, next) => {
+//   console.log("POST /monitoring/add/:MachineId 진입");
+//   console.log(req.body);
+//   try {
+//     const MachineId = parseInt(req.params.MachineId, 10);
+//     /*
+//     1) 1호기, 2호기, 3호기 각 결과값이 통과했는지 여부를 어디 DB에 update.
+//     2) final_result를 Test_result DB에 add.
+//    (1, 2, 3호기 part_status가 전부 ‘정상’이어야 final result가 pass.  / 아니면 fail 이 되도록 로직)
+//     */
+//   } catch (error) {
+//     console.error(error);
+//     next(error);
+//   }
+// });
+
+module.exports = router;

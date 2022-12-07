b953a85b600ac1fbdb56496eac3897b8ba658065
Lee-June-Kyu <leejuk95@naver.com>
2022-12-07 19:51:03 +0900

    Update Login Function2

--- FrontEnd/src/store/Auth.js
+++ FrontEnd/src/store/Auth.js
@@ -1,12 +1,44 @@
-// import { auth } from '../api/auth'
+import { auth } from '../api/auth'
+import { setAuthInHeader } from '../api/auth'
+
+// 유저 정보 수정 필요
+const InitTokenUser = {
+    id: null,
+    employee_number: null,
+    name: null,
+    email: null,
+    authorization: null
+}
+
 
 export const Auth = {
   namespaced: true,
 
-  state: {},
-  // getters:{
+  state: {
+    TokenUser : InitTokenUser
+  },
+  getters:{
+    TokenUser : state => state.TokenUser
+  },
+  mutations: {
+    LOGIN (state, data) {
+      if (!data.token) return
+      // 여기 아래 user에 대한 내용을 넣어야 한다.
+      state.TokenUser = data.user //token 갱신
+      localStorage.setItem('token', data.token) //localstorage에 token 저장
 
-  // },
-  mutations: {},
-  actions: {}
+      setAuthInHeader(data.token) //header에 token 세팅
+    }
+  },
+  actions: {
+    LOGIN_AUTH({commit}, {employee_number, password}) {
+      return auth.login(employee_number,password).then((res)=>{
+        console.log('Login 성공 : ', res);
+      }).catch((res)=>{
+        console.log('Login 실패 : ', res);
+      }).finally(()=>{
+        console.log('final');
+      })
+    }
+  }
 }

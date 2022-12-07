2097ced35e34ae0aec7c2fdaef61c88449b7dccd
_devOck <respecttowardyou@gmail.com>
2022-12-07 16:40:04 +0900

    Update routes/line Code

--- BackEnd/routes/line.js
+++ BackEnd/routes/line.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../controllers/middlewares");
 
-const Machine = require("../models/machine");
+const { Machine } = require("../models");
 
 // 공정 전체 정보 조회
 router.GET("/", verifyToken, async (req, res, next) => {
   console.log("GET / 진입");
-  const machine = await Machine.findAll();
   try {
+    const machine = await Machine.findAll();
+    if (!machine) {
+      return res.status(resStatus.insufficient.code).json({
+        message: resStatus.insufficient.message, // (204) 보낼 data가 없거나 부족할 때
+      });
+    }
     return res.status(resStatus.success.code).json({
       message: resStatus.success.message, // (200) success
       data: { machine },

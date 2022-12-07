364e90f7e9c56962f2d5ed5f6f45a32b55f057f6
Euntaek13 <euntaekc13@gmail.com>
2022-12-07 21:21:11 +0900

    Create Item constructor of Line

--- FrontEnd/src/views/Line.vue
+++ FrontEnd/src/views/Line.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <h3>Line Page</h3>
+    <h1>Line Page</h1>
+    <Item>
+        <div slot="title"><h3>Title</h3></div>
+        <div slot="subtitle"><p>subtitle</p></div>
+        <div slot="description"><p>body</p></div>
+        <div slot="footer">
+          <router-link to="/monitoring" style="text-decoration: none;">
+            <v-btn>monitoring</v-btn>
+          </router-link>
+        </div>
+    </Item>
   </div>
 </template>
 
 <script>
+import Item from '../components/Item.vue'
   export default {
     name:'line',
+    components:{
+      Item
+    },
     data(){
       return {
 
@@ -16,5 +30,7 @@
 </script>
 
 <style lang="sass" scoped>
+
+
 
 </style>

4f6b9d99977a630fc7961eebb7a6273fb192326a
_devOck <respecttowardyou@gmail.com>
2022-12-07 16:39:49 +0900

    Update routes/auth Code

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -7,22 +7,22 @@ const User = require("../models/user");
 const { resStatus } = require("../lib/responseStatus");
 
 // 로그인
-router.GET("/login", async (req, res, next) => {
+router.GET("/", async (req, res, next) => {
   console.log("GET /login 진입");
   console.log(req.body);
-  const { employee_number, password } = req.body;
-  const user = await User.findOne({ where: { employee_number } });
   try {
+    const { employee_number, password } = req.body;
+    const user = await User.findOne({ where: { employee_number } });
     if (!employee_number || !password) {
       return res.status(resStatus.notenough.code).json({
-        message: resStatus.notenough.message, // (404) 원하는 data가 req에 아예 없거나 부족할 때
+        message: resStatus.notenough.message, // (204) 원하는 data가 param이나 req에 아예 없거나 부족할 때
       });
     }
 
     const result = await bcrypt.compare(password, user.password);
     if (!result) {
-      return res.status(resStatus.invalidp.code).json({
-        message: resStatus.invalidp.message, // (404) invalid pw
+      return res.status(resStatus.invalid.code).json({
+        message: resStatus.invalid.message, // (204) req로 받은 data가 유효하지 않을 때
       });
     } else {
       const token = jwt.sign(
@@ -38,6 +38,11 @@ router.GET("/login", async (req, res, next) => {
           issuer: "KYOL",
         }
       );
+      const user = await User.findOne({
+        where: { employee_number },
+        attributes: ["id", "employee_number", "email", "name", "authorization"],
+      });
+
       return res.status(resStatus.success.code).json({
         message: resStatus.success.message, // (200) success
         token,

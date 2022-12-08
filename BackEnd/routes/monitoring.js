808eee6571694b438c0d56d8c534ef2918a438b7
_devOck <respecttowardyou@gmail.com>
2022-12-09 04:52:53 +0900

    Update monitoring Code

--- BackEnd/routes/monitoring.js
+++ BackEnd/routes/monitoring.js
@@ -4,10 +4,20 @@ const { verifyToken } = require("../controllers/middlewares");
 
 const {
   monitoringExplorer,
-  monitoringInformationAdd,
+  partUpdate,
+  machineStatusUpdate,
+  testResultAdd
 } = require("../controllers/monitoring");
 
 router.get("/:MachineId", verifyToken, monitoringExplorer); // 공정 모니터링 정보 조회
-// router.post("/add/:MachineId", verifyToken, monitoringInformationAdd); // 공정 모니터링 실시간 정보 추가
+
+// router.post("/add/:MachineId",  partUpdate); // 공정 모니터링 part 실시간 정보 업데이트 
+
+router.post("/update/part/:MachineId",  partUpdate); // 공정 모니터링 part 실시간 정보 업데이트 
+// router.post("/update/part/:MachineId", verifyToken, partUpdate); // 공정 모니터링 part 실시간 정보 업데이트 
+
+// router.post("/update/machine/:MachineId", verifyToken, machineStatusUpdate) // 공정 status 업데이트
+
+// router.post("/add/result/:MachineId", verifyToken, testResultAdd) // test result 추가
 
 module.exports = router;

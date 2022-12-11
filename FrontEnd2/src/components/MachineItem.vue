8df4df914155dd56257f39e4469bc8ddc98d73b8
Lee-June-Kyu <leejuk95@naver.com>
2022-12-12 08:59:52 +0900

    Create Machines Page UI : Create Machines Page Layout

---
+++ FrontEnd2/src/components/MachineItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="machine-container"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.machine-container {
+  width: 30%;
+  height: 80%;
+  position: relative;
+  border: 1px solid black;
+  margin-right: 3%;
+}
+
+.machine-container:after {
+  content: '';
+  display: block;
+  padding-bottom: 100%;
+}
+</style>

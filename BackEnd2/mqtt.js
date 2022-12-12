8b2efdeb1576f66506dcee37d24e5ad279f7f47a
_devOck <respecttowardyou@gmail.com>
2022-12-12 17:15:01 +0900

    Update mtqq code

--- BackEnd2/mqtt.js
+++ BackEnd2/mqtt.js
@@ -13,6 +13,10 @@ sequelize
     console.error(err);
   });
 
+// 전역변수
+let flag1 = false;
+let flag2 = false;
+
 // const client = mqtt.connect("mqtt://localhost:1883"); // connect PLC directly
 const client = mqtt.connect("mqtt://192.168.0.72:1883"); // connect PLC through JK smart connector
 
@@ -29,36 +33,134 @@ client.on("connect", () => {
 // 네 가능합니다 ↓↓↓
 
 // 두 topic에 대한 subscribe
-client.subscribe(["machine", "machine2"]);
+// client.subscribe(["machine", "machine2"]);
+client.subscribe("machine1");
 
 // subscribe에 대한 message 받기. 각 topic에 따른 로직 처리
 client.on("message", (topic, message, packet) => {
-  console.log("###########");
+  // console.log("###########");
 
   // message가 buffer로 오므로 JSON으로 변환
   const rareElements = JSON.parse(message.toString());
-
   // tag의 number로 해당 tag의 정보를 부를 수 있게 tag의 number를 키값으로 값는 객체로 재정리
   const machineElements = rareElements.Wrapper;
-  // console.log(machineElements[0].tagId)
-  const elements = {};
-  for (i = 0; i < machineElements.length; i++) {
-    const tagNumber = machineElements[i].tagId;
-    elements[tagNumber] = machineElements[i];
+  // console.log(machineElements);
+
+  // console.log("orign : ", machineElements);
+
+  const machineElementsSorts = machineElements.sort((a, b) => {
+    if (parseInt(a.tagId) > parseInt(b.tagId)) {
+      return 1;
+    } else if (parseInt(a.tagId) < parseInt(b.tagId)) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  if (machineElementsSorts[6].value === true) {
+    flag1 = true;
   }
 
-  // 받은 데이터 모두 DB에 저장
-  for (i = 0; i < machineElements.length; i++) {
-    if (elements[i + 1]) {
-      // 어떤 데이터들은 전송되지 않았을 수도 있어서 없는 key값도 있기 때문에 필터해줘야 한다.
-      Element.create({
-        tagId: i + 1,
-        name: elements[i + 1]["name"],
-        value: elements[i + 1].value,
-      });
-      console.log(`${elements[i + 1]["name"]}에 대한 정보가 DB에 추가`);
+  if (machineElementsSorts[18].value === true) {
+    flag2 = true;
+    if (flag1 === flag2) {
+      console.log("###########################");
+      console.log("6번 true");
+      flag1 = false;
+      flag2 = false;
+    } else {
+      console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+      console.log("6번 false");
+      flag1 = false;
+      flag2 = false;
     }
   }
+
+  // 20 true -> 6번 true / false
+  // if (machineElementsSorts[20].value === true) {
+  //   if (machineElementsSorts[6].value === true) {
+  //     console.log("6번 true");
+  //   } else if (machineElementsSorts[6].value === false) {
+  //     console.log("6번 false");
+  //   } else {
+  //     console.log("아님");
+  //   }
+  // }
+
+  // // 받은 데이터 모두 DB에 저장
+  // machineElementsSorts.map((i) =>
+  //   Element.create({
+  //     tagId: i.tagId,
+  //     name: i.name,
+  //     value: i.value,
+  //   })
+  // );
+
+  // for (i = 0; i < machineElements.length; i++) {
+  //   if (elements[i + 1]) {
+  //     // 어떤 데이터들은 전송되지 않았을 수도 있어서 없는 key값도 있기 때문에 필터해줘야 한다.
+  //     Element.create({
+  //       tagId: i + 1,
+  //       name: elements[i + 1]["name"],
+  //       value: elements[i + 1].value,
+  //     });
+  //     console.log(`${elements[i + 1]["name"]}에 대한 정보가 DB에 추가`);
+  //   }
+  // }
+
+  // console.log("tagId 오름차순 : ", machineElementsSorts);
+
+  // machineElementsSorts.map((i) =>
+  //   // tagId 값에 따라 DB에 저장
+  //   // 로직할 것이 있으면 먼저 하고 DB에 저장해야 함
+  //   // 아래는 예시
+  //   switch (parseInt(i.tagId)){
+  //     case 6:
+  //       로직~~~~~~~~~~~~~
+  //       테이블명.create({
+  //         컬럼명: i.tagId,
+  //         컬럼명: i.name,
+  //         컬럼명: i.value,
+  //       })
+  //       break;
+  //     case 1,3,7:
+  //       테이블명.create({
+  //         컬럼명: i.tagId,
+  //         컬럼명: i.name,
+  //         컬럼명: i.value,
+  //       })
+  //       break;
+  //     case 1,3,7:
+  //       테이블명.create({
+  //         컬럼명: i.tagId,
+  //         컬럼명: i.name,
+  //         컬럼명: i.value,
+  //       })
+  //       break;
+  //     case 1,3,7:
+  //       테이블명.create({
+  //         컬럼명: i.tagId,
+  //         컬럼명: i.name,
+  //         컬럼명: i.value,
+  //       })
+  //       break;
+  //     case 1,3,7:
+  //       테이블명.create({
+  //         컬럼명: i.tagId,
+  //         컬럼명: i.name,
+  //         컬럼명: i.value,
+  //       })
+  //       break;
+  //     case 1,3,7:
+  //       테이블명.create({
+  //         컬럼명: i.tagId,
+  //         컬럼명: i.name,
+  //         컬럼명: i.value,
+  //       })
+  //   }
+  //   console.log(i)
+  // );
 
   // var today = new Date();
 
@@ -70,7 +172,7 @@ client.on("message", (topic, message, packet) => {
   //   value: elements[3].value,
   // });
 
-  console.log("the end");
+  // console.log("the end");
 });
 
 // error 처리
@@ -81,7 +183,7 @@ client.on("error", (error) => {
 
 //////////////////////////////
 
-// // 두 topic에 대한 subscribe
+// 두 topic에 대한 subscribe
 // client.subscribe(["machine", "machine2"]);
 
 // // subscribe에 대한 message 받기. 각 topic에 따른 로직 처리

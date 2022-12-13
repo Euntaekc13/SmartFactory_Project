e496a221056f6cd87ec7efd1b93a750b0108d238
Lee-June-Kyu <leejuk95@naver.com>
2022-12-14 00:26:36 +0900

    Update History Page UI : Content__Up area

--- FrontEnd2/src/views/History.vue
+++ FrontEnd2/src/views/History.vue
@@ -1,26 +1,70 @@
 <template>
-  <div>
+  <div style="max-height: 100vh; box-sizing: border-box">
     <Navbar></Navbar>
     <v-main>
       <div class="history__header">
-        <h1>History Data List ({{ countAll }})</h1>
-        <v-divider vertical></v-divider>
+        <h1>
+          History Data List <span class="header__count">({{ countAll }})</span>
+        </h1>
+        <!-- <v-divider vertical></v-divider> -->
         <!-- <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field> -->
       </div>
-      <v-divider></v-divider>
-      <div>
-        <input type="text" submit />
-        <button submit>check</button>
-      </div>
-      <button type="button" @click="excelDown">엑셀다운</button>
-
-      <v-card>
-        <v-card-title>
-          <!-- <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details>
+      <div class="history__content">
+        <!-- <input type="text" submit />
+        <button submit>check</button> -->
+        <div class="history__content__up">
+          <div class="history_content_icons">
+            <button type="button" @click="excelDown">
+              <span class="icon-button excel">
+                <i class="fa-solid fa-file-arrow-down icon-excel"></i><span></span>
+              </span>
+            </button>
+            &nbsp;
+            <button type="button" @click="excelDown">
+              <span class="icon-button repeat"> <i class="fa-solid fa-retweet icon-repeat"></i><span></span> </span>
+            </button>
+          </div>
+          <div class="history_content_condition">
+            <!-- <span>date picker</span>&nbsp; -->
+            <div>
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                :return-value.sync="date"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateRangeText"
+                    label="Picker in menu"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="dates" range no-title>
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+                  <v-btn text color="primary" @click="$refs.menu.save(dates)"> OK </v-btn>
+                </v-date-picker>
+              </v-menu>
+            </div>
+            <span>공정 선택</span>&nbsp;
+            <span>양품/고품</span>
+          </div>
+        </div>
+        <v-card class="histroy__card" elevation="4" max-height="100vh">
+          <v-card-title>
+            <!-- <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details>
           </v-text-field> -->
-        </v-card-title>
-        <v-data-table :headers="headers" :items="details" :search="search"></v-data-table>
-      </v-card>
+          </v-card-title>
+          <v-data-table :headers="headers" :items="details" :search="search" :items-per-page="8"></v-data-table>
+        </v-card>
+      </div>
     </v-main>
   </div>
 </template>
@@ -36,6 +80,14 @@ export default {
     return {
       search: '',
       countAll: 0,
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+      menu: false,
+      modal: false,
+      menu2: false,
+      dates: [
+        new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+        new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10)
+      ],
       headers: [
         {
           text: 'Serial information',
@@ -133,6 +185,11 @@ export default {
       ]
     }
   },
+  computed: {
+    dateRangeText() {
+      return this.dates.join(' ~ ')
+    }
+  },
   methods: {
     async excelDown() {
       const xlsx = require('xlsx')
@@ -177,8 +234,119 @@ export default {
 </script>
 
 <style scoped>
+* {
+  background-color: white;
+}
 .history__header {
   display: flex;
-  background-color: red;
+  padding: 2% 4% 2% 4%;
+  border-bottom: 2px solid #e0e0e0;
+  /* background-color: red; */
+}
+.header__count {
+  color: #e0e0e0;
+  font-size: 0.7em;
+}
+.history__content {
+  box-sizing: border-box;
+  height: 80vh;
+  padding: 1% 4% 2% 4%;
+}
+.histroy__card {
+  box-sizing: border-box;
+}
+.history__content__up {
+  /* background-color: red; */
+  padding: 0% 0% 0% 0%;
+  display: flex;
+  justify-content: space-between;
+}
+.icon-button {
+  background-color: white;
+  border-radius: 3.6rem;
+  cursor: pointer;
+  display: inline-block;
+  font-size: 2rem;
+  height: 3.6rem;
+  line-height: 3.6rem;
+  margin: 0 5px;
+  position: relative;
+  text-align: center;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  width: 3.6rem;
+}
+/* .excel {
+  border: 2px solid #1e6f43;
+} */
+/* .repeat {
+  border: 2px solid #3b5998;
+} */
+
+/* Circle */
+.icon-button span {
+  /* background-color: #1e6f43; */
+  /* background-color: red; */
+  border-radius: 0;
+  display: block;
+  height: 0;
+  left: 50%;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -o-transition: all 0.3s;
+  transition: all 0.3s;
+  width: 0;
+}
+.icon-button:hover span {
+  width: 3.6rem;
+  height: 3.6rem;
+  border-radius: 3.6rem;
+  margin: -1.8rem;
+}
+.excel span {
+  background-color: #1e6f43;
+}
+.repeat span {
+  background-color: #3b5998;
+}
+
+/* Icons */
+.icon-button i {
+  background: none;
+  color: white;
+  height: 3.6rem;
+  left: 0;
+  line-height: 3.6rem;
+  position: absolute;
+  top: 0;
+  -webkit-transition: all 0.3s;
+  -moz-transition: all 0.3s;
+  -o-transition: all 0.3s;
+  transition: all 0.3s;
+  width: 3.6rem;
+  z-index: 10;
+}
+.icon-button .icon-excel {
+  color: #1e6f43;
+}
+.icon-button .icon-repeat {
+  color: #3b5998;
+}
+
+.icon-button:hover .icon-excel {
+  color: white;
+}
+
+.icon-button:hover .icon-repeat {
+  color: white;
+}
+
+.icon-button .icon-repeat {
+  color: #3b5998;
 }
 </style>

7a18deb6a298c9ba35ee623c2047a9aca705762b
Lee-June-Kyu <leejuk95@naver.com>
2022-12-14 16:53:37 +0900

    Create History API

---
+++ FrontEnd2/src/api/history.js
@@ -0,0 +1,8 @@
+import { request } from './index'
+
+export const history = {
+  getHistoryData(historyData) {
+    console.log('get History Data information')
+    return request('post', '/history', { historyData })
+  }
+}

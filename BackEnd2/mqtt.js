ad75ee170be4d0ff025b66eb7bac054ef6b3292a
_devOck <respecttowardyou@gmail.com>
2022-12-18 04:34:19 +0900

    Update mqtt code(dice_num after 2nd part)

--- BackEnd2/mqtt.js
+++ BackEnd2/mqtt.js
@@ -19,6 +19,7 @@ let goodsetFlag1 = false; // 색깔 판별해서 흰색이면 true
 let goodsetFlag2 = false; // 2호기에 도달했을 때 true
 let final_result = null;
 let dice_num = 0;
+let arr = [];
 
 // const client = mqtt.connect("mqtt://localhost:1883"); // connect PLC directly
 const client = mqtt.connect("mqtt://192.168.0.72:1883"); // connect PLC through JK smart connector
@@ -31,7 +32,7 @@ client.subscribe("machine");
 
 // subscribe에 대한 message 받기. 각 topic에 따른 로직 처리
 client.on("message", async (topic, message, packet) => {
-  // console.log("###########");
+  console.log("###########");
   try {
     // message가 buffer로 오므로 JSON 형식으로 변환
     const rareElements = await JSON.parse(message.toString());
@@ -46,6 +47,7 @@ client.on("message", async (topic, message, packet) => {
         return 0;
       }
     });
+    // console.log(machineElementsSorts);
     if (
       // tag 26(belt)가 true이거나 tag 1(Start)이 true이면, machine_status : 1 (동작 중)
       machineElementsSorts[16].value == true ||
@@ -79,23 +81,46 @@ client.on("message", async (topic, message, packet) => {
       if (machineElementsSorts[11].value == true) {
         goodsetFlag2 = true;
       }
+      // setTimeout(() => {
+      //   if (machineElementsSorts[14].value > 0) {
+      //     arr.push(machineElementsSorts[14].value);
+      //   }
+      // }, 1200);
+
+      // 주사위 눈 값 배열에 담고, 가장 빈도수 높은 값 dice_num2에 담기
+      if (machineElementsSorts[14].value > 0) {
+        arr.push(machineElementsSorts[14].value);
+      }
+      const result = arr.reduce((accu, curr) => {
+        accu[curr] = (accu[curr] || 0) + 1;
+        return accu;
+      }, {});
+      console.log(result);
+      if (result) {
+        let dice_num2 = Object.keys(result).reduce((a, b) =>
+          result[a] > result[b] ? a : b
+        );
+        console.log("dice_num2 : ", dice_num2);
+      }
       // tag 25(No3Gripper)이 true이고, thirdFlag가 true이면,
       if (machineElementsSorts[15].value == true && thirdFlag == true) {
         // await finalProcess(machineElementsSorts);
         // 1) 변수 재설정
         let secondFlag_t = secondFlag;
-        let goodsetFlag1_t = goodsetFlag1;
-        let goodsetFlag2_t = goodsetFlag2;
+        // let goodsetFlag1_t = goodsetFlag1;
+        // let goodsetFlag2_t = goodsetFlag2;
         firstFlag = false;
         secondFlag = false;
         thirdFlag = false;
-        goodsetFlag1 = false;
-        goodsetFlag2 = false;
+        // goodsetFlag1 = false;
+        // goodsetFlag2 = false;
+
         // 2) 1,2,3호기 DB count +=1
         await Part.increment({ count: 1 }, { where: { PartDefaultId: 1 } });
         if (secondFlag_t == true) {
           await Part.increment({ count: 1 }, { where: { PartDefaultId: 2 } });
           final_result = 1;
+          dice_num = dice_num2;
         } else {
           final_result = 2;
         }
